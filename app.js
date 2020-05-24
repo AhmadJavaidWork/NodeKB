9ba32d8fe97c9f8e0ec1121cd102b6f7e49e105f
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2020-05-24 15:58:14 +0500

    connected mongodb and created article model

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+})
+
+// Check for DB errors
+db.on('error', (err) => {
+  console.log(err);
+})
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -11,29 +27,15 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (req, res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Article One',
-      author: 'Ahmad Javaid',
-      body: 'This is article one'
-    },
-    {
-      id: 2,
-      title: 'Article Two',
-      author: 'John Does',
-      body: 'This is article two'
-    },
-    {
-      id: 1,
-      title: 'Article Three',
-      author: 'Jacob',
-      body: 'This is article three'
+  Article.find({}, (err, articles) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
     }
-  ];
-  res.render('index', {
-    title: 'Articles',
-    articles: articles
   });
 });
 
