de196d2a60b8be0649a9fc394a1fbc225f5c58e2
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2020-05-24 19:47:36 +0500

    implemented login, registeration and authentication with Passport

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# NodeKB
-To run the app:
+Technologies
+Node.js
+Express
+Express Messages, Session, Connect Flash & Validation
+MongoDB & Mongoose
+Pug Templating
+Passport.js Authentication
+BCrypt Hashing
+Version
+2.0.0
 
-npm install
+Usage
+Installation
+Install the dependencies
 
-npm start
+$ npm install
+Run app
+
+$ npm start
--- app.js
+++ app.js
@@ -5,19 +5,21 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
 
-mongoose.connect('mongodb://localhost/nodekb');
+mongoose.connect(config.database);
 let db = mongoose.connection;
 
 // Check connection
-db.once('open', () => {
+db.once('open', function(){
   console.log('Connected to MongoDB');
-})
+});
 
 // Check for DB errors
-db.on('error', (err) => {
+db.on('error', function(err){
   console.log(err);
-})
+});
 
 // Init App
 const app = express();
@@ -29,152 +31,79 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Body Parseer Middleware
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+// parse application/json
+app.use(bodyParser.json());
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Express Session Midddleware
+// Express Session Middleware
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true
 }));
 
-// Express Messages Midddleware
-app.use(flash());
-app.use((req, res, next) => {
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
 // Express Validator Middleware
 app.use(expressValidator({
-  errorFormatter: (param, msg, value) => {
-    var namespace = param.split('.')
-    , root = namespace.shift()
-    , formParam = root;
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
     while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param: formParam,
-      msg: msg,
-      value: value
+      param : formParam,
+      msg   : msg,
+      value : value
     };
   }
-}))
+}));
+
+// Passport Config
+require('./config/passport')(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Home Route
-app.get('/', (req, res) => {
-  Article.find({}, (err, articles) => {
-    if (err) {
+app.get('/', function(req, res){
+  Article.find({}, function(err, articles){
+    if(err){
       console.log(err);
     } else {
       res.render('index', {
-        title: 'Articles',
+        title:'Articles',
         articles: articles
       });
     }
   });
 });
 
-// Get Single Article
-app.get('/article/:id', (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render('article', {
-      article: article
-    });
-  });
-});
-
-// Add Route
-app.get('/articles/add', (req, res) => {
-  res.render('add_article', {
-    title: 'Add Article'
-  });
-});
-
-// Add  Submit POST Route
-app.post('/articles/add', (req, res) => {
-  req.checkBody('title', 'Title is required').notEmpty();
-  req.checkBody('author', 'Author is required').notEmpty();
-  req.checkBody('body', 'Body is required').notEmpty();
-
-  // Get Errors
-  let errors = req.validationErros();
-
-  if (errors) {
-    res.render('add_article', {
-      title: 'Add Article',
-      errors: errors
-    });
-  } else {
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-
-    article.save((err) => {
-      if (err) {
-        console.log(err);
-        return;
-      } else {
-        req.flash('success', 'Article Added');
-        res.redirect('/');
-      }
-    });
-  }
-});
-
-// Load Edit Form
-app.get('/article/edit/:id', (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render('edit_article', {
-      title: 'Edit Artricle',
-      article: article
-    });
-  });
-});
-
-// Update  Submit POST Route
-app.post('/article/edit/:id', (req, res) => {
-  let article = {}
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = {_id:req.params.id};
-
-  Article.update(query, article, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Article Updated');
-      res.redirect('/');
-    }
-  });
-});
-
-// Delete Route
-app.delete('/article/:id', (req, res) => {
-  let query = {_id:req.params.id}
-
-  Article.remove(query, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      res.send('Success');
-    }
-  });
-});
+// Route Files
+let articles = require('./routes/articles');
+let users = require('./routes/users');
+app.use('/articles', articles);
+app.use('/users', users);
 
 // Start Server
-app.listen(3000, () => {
-  console.log('Server started on port 3000...')
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
 });
