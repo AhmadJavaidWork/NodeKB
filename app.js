766444fccd0281d09a00d384283f2d70cd6bbbc2
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2020-05-24 15:35:08 +0500

    added articles and add_article page

--- app.js
+++ app.js
@@ -11,8 +11,39 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (req, res) => {
-  res.render('index');
+  let articles = [
+    {
+      id: 1,
+      title: 'Article One',
+      author: 'Ahmad Javaid',
+      body: 'This is article one'
+    },
+    {
+      id: 2,
+      title: 'Article Two',
+      author: 'John Does',
+      body: 'This is article two'
+    },
+    {
+      id: 1,
+      title: 'Article Three',
+      author: 'Jacob',
+      body: 'This is article three'
+    }
+  ];
+  res.render('index', {
+    title: 'Articles',
+    articles: articles
+  });
 });
+
+// Add Route
+app.get('/articles/add', (req, res) => {
+  res.render('add_article', {
+    title: 'Add Article'
+  });
+});
+
 
 
 // Start Server
