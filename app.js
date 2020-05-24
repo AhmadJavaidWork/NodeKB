c7ddb3269b7a0d5231a8d4218a44b55f40b842ab
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2020-05-24 18:40:02 +0500

    implemented full CRUD functionality with validator, flash and session

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -30,6 +33,41 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Midddleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Midddleware
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}))
+
 // Home Route
 app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
@@ -44,6 +82,15 @@ app.get('/', (req, res) => {
   });
 });
 
+// Get Single Article
+app.get('/article/:id', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('article', {
+      article: article
+    });
+  });
+});
+
 // Add Route
 app.get('/articles/add', (req, res) => {
   res.render('add_article', {
@@ -53,17 +100,76 @@ app.get('/articles/add', (req, res) => {
 
 // Add  Submit POST Route
 app.post('/articles/add', (req, res) => {
-  let article = new Article();
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('author', 'Author is required').notEmpty();
+  req.checkBody('body', 'Body is required').notEmpty();
+
+  // Get Errors
+  let errors = req.validationErros();
+
+  if (errors) {
+    res.render('add_article', {
+      title: 'Add Article',
+      errors: errors
+    });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        req.flash('success', 'Article Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// Load Edit Form
+app.get('/article/edit/:id', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('edit_article', {
+      title: 'Edit Artricle',
+      article: article
+    });
+  });
+});
+
+// Update  Submit POST Route
+app.post('/article/edit/:id', (req, res) => {
+  let article = {}
   article.title = req.body.title;
   article.author = req.body.author;
   article.body = req.body.body;
 
-  article.save((err) => {
+  let query = {_id:req.params.id};
+
+  Article.update(query, article, (err) => {
     if (err) {
       console.log(err);
       return;
     } else {
+      req.flash('success', 'Article Updated');
       res.redirect('/');
+    }
+  });
+});
+
+// Delete Route
+app.delete('/article/:id', (req, res) => {
+  let query = {_id:req.params.id}
+
+  Article.remove(query, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.send('Success');
     }
   });
 });
@@ -71,4 +177,4 @@ app.post('/articles/add', (req, res) => {
 // Start Server
 app.listen(3000, () => {
   console.log('Server started on port 3000...')
-})
+});
