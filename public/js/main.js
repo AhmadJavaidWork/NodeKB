146628c052d92b9132395da0e8749534f3c2901e
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2020-05-24 20:05:43 +0500

    implemented login, registeration and authentication with Passport

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(() => {
-  $('.delete-article').on('click', (e) => {
+$(document).ready(function(){
+  $('.delete-article').on('click', function(e){
     $target = $(e.target);
     const id = $target.attr('data-id');
     $.ajax({
       type:'DELETE',
-      url: '/article/'+id,
-      success: (response) => {
+      url: '/articles/'+id,
+      success: function(response){
         alert('Deleting Article');
         window.location.href='/';
       },
-      error: (err) => {
+      error: function(err){
         console.log(err);
       }
     });
